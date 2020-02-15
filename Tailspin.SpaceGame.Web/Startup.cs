4a25bcc74e3782d9a7c1e73d7ed59234f45f8155
Hiten Mahida <hitendrasinh.mahida@bell.ca>
2020-02-15 18:09:15 -0500

    Fixed merge issues

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-<<<<<<< HEAD
 using TailSpin.SpaceGame.Web.Models;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-=======
-using Microsoft.Extensions.Hosting;
-using TailSpin.SpaceGame.Web.Models;
-using Microsoft.AspNetCore.Http;
-
->>>>>>> master
 
 namespace TailSpin.SpaceGame.Web
 {
@@ -60,22 +50,12 @@ namespace TailSpin.SpaceGame.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-<<<<<<< HEAD
             app.UseCookiePolicy();
             app.UseRouting();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
              {
-=======
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
->>>>>>> master
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
